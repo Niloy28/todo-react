b6c002b729556dd1fef1a87549c4da7ef8713228
Farhan Ishrak Islam <Niloy28@live.com>
2022-04-15 22:43:44 +0600

    Improve styling

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ function App(props) {
         3 tasks remaining
       </h2>
       <ul
+        role='list'
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
