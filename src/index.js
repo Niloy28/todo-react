7315832f60ddf9bf5da54a48410a23b3c3116da8
Farhan Ishrak Islam <niloy28@live.com>
2022-04-17 18:28:36 +0600

    Componentisize forms and buttons.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const DATA = [
+	{ id: "todo-0", name: "Eat", completed: true },
+	{ id: "todo-1", name: "Sleep", completed: false },
+	{ id: "todo-2", name: "Repeat", completed: false },
+];
+
+const BUTTONS = [
+	{ filter: "all" },
+	{ filter: "active" },
+	{ filter: "completed" },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App tasks={DATA} buttons={BUTTONS} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
