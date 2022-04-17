81aff85e094135c2a0ed3c1e8deef4a836c9dfd4
Farhan Ishrak Islam <Niloy28@live.com>
2022-04-17 22:57:09 +0600

    Can now add and delete task items

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
-import Todo from "./components/Todo";
-import Form from "./components/Form";
-import FilterButton from "./components/FilterButton";
+import React, { useState } from 'react';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import { nanoid } from 'nanoid';
 
 function App(props) {
-	function addTask(name) {
-		alert(name);
-	}
+  const [tasks, setTasks] = useState(props.tasks);
 
-	const taskList = props.tasks.map((task) => (
-		<Todo
-			name={task.name}
-			completed={task.completed}
-			id={task.id}
-			key={task.id}
-		/>
-	));
+  function addTask(name) {
+    const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
 
-	const buttonList = props.buttons.map((button) => (
-		<FilterButton filter={button.filter} key={button.filter} />
-	));
-	return (
-		<div className="todoapp stack-large">
-			<h1>TodoMatic</h1>
-			<Form addTask={addTask} />
-			<div className="filters btn-group stack-exception">{buttonList}</div>
-			<h2 id="list-heading">3 tasks remaining</h2>
-			<ul
-				role="list"
-				className="todo-list stack-large stack-exception"
-				aria-labelledby="list-heading"
-			>
-				{taskList}
-			</ul>
-		</div>
-	);
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => task.id !== id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      name={task.name}
+      completed={task.completed}
+      id={task.id}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+  const taskNoun = taskList.length === 1 ? 'task' : 'tasks';
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
+
+  const buttonList = props.buttons.map((button) => (
+    <FilterButton filter={button.filter} key={button.filter} />
+  ));
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">{buttonList}</div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Form(props) {
-	function handleSubmit(e) {
-		e.preventDefault();
-		props.addTask("Say hello");
-	}
+  const [name, setName] = useState('');
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<h2 className="label-wrapper">
-				<label htmlFor="new-todo-input" className="label__lg">
-					What needs to be done?
-				</label>
-			</h2>
-			<input
-				type="text"
-				id="new-todo-input"
-				className="input input__lg"
-				name="text"
-				autoComplete="off"
-			/>
-			<button type="submit" className="btn btn__primary btn__lg">
-				Add
-			</button>
-		</form>
-	);
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (name.trim() !== '') {
+      props.addTask(name);
+    }
+
+    setName('');
+  }
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        onChange={handleChange}
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default Form;
